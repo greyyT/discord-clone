54e12da464a757f06278a722b13c0420fa0ff322
greyyT <nhannguyen8338@gmail.com>
2023-11-18 12:44:58 +0700

    Add edit server modal for editing server image and name

--- components/modals/index.ts
+++ components/modals/index.ts
@@ -1,5 +1,6 @@
 import { InitialModal } from './initial-modal';
 import { CreateServerModal } from './create-server-modal';
 import { InviteModal } from './invite-modal';
+import { EditServerModal } from './edit-server-modal';
 
-export { InitialModal, CreateServerModal, InviteModal };
+export { InitialModal, CreateServerModal, InviteModal, EditServerModal };
--- hooks/modal-store/modal-type.d.ts
+++ hooks/modal-store/modal-type.d.ts
@@ -1,1 +1,1 @@
-export type ModalType = 'createServer' | 'invite';
+export type ModalType = 'createServer' | 'invite' | 'editServer';
