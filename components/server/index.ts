a9299a977d8173908869d8dc20f374029622c604
greyyT <nhannguyen8338@gmail.com>
2023-11-21 18:28:45 +0700

    Create server-channel and server-member components for displaying channels and members on the sidebar

--- components/server/index.ts
+++ components/server/index.ts
@@ -2,5 +2,7 @@ import ServerSidebar from './server-sidebar';
 import ServerHeader from './server-header';
 import ServerSearch from './server-search';
 import ServerSection from './server-section';
+import ServerChannel from './server-channel';
+import ServerMember from './server-member';
 
-export { ServerSidebar, ServerHeader, ServerSearch, ServerSection };
+export { ServerSidebar, ServerHeader, ServerSearch, ServerSection, ServerChannel, ServerMember };
---
+++ components/server/server-member.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { Member, MemberRole, Profile, Server } from '@prisma/client';
+import { ShieldAlert, ShieldCheck } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
+import { UserAvatar } from '../user-avatar';
+
+interface ServerMemberProps {
+  member: Member & { profile: Profile };
+  server: Server;
+}
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 text-indigo-500" />,
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 text-rose-500" />,
+};
+
+const ServerMember: React.FC<ServerMemberProps> = ({ member, server }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const icon = roleIconMap[member.role];
+
+  return (
+    <button
+      className={cn(
+        'group',
+        'p-2',
+        'rounded-md',
+        'flex items-center gap-x-2',
+        'w-full',
+        'hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50',
+        'transition',
+        'mb-1',
+        params?.memberId === member.id && 'bg-zinc-700/20 dark:bg-zinc-700',
+      )}
+    >
+      <UserAvatar src={member.profile.imageUrl} className="h-8 w-8 md:h-8 md:w-8" />
+      <p
+        className={cn(
+          'font-semibold',
+          'text-sm text-zinc-500 dark:text-zinc-400',
+          'group-hover:text-zinc-600 dark:group-hover:text-zinc-300',
+          'transition',
+          params?.channelId === member.id && 'text-primary dark:text-zinc-200 dark:group-hover:text-white',
+        )}
+      >
+        {member.profile.name}
+      </p>
+      {icon}
+    </button>
+  );
+};
+
+export default ServerMember;
