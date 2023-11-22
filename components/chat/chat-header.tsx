2436dc0a9a90dd19e5d5f88ec7f9e6911baab8b4
greyyT <nhannguyen8338@gmail.com>
2023-11-22 21:05:48 +0700

    Create socket-indicator component for displaying user connectivity to socket io

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -3,6 +3,7 @@ import { Hash, Menu } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { MobileToggle } from '@/components/mobile-toggle';
 import { UserAvatar } from '@/components/user-avatar';
+import { SocketIndicator } from '../socket-indicator';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -27,6 +28,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
       {type === 'channel' && <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />}
       {type === 'conversation' && <UserAvatar src={imageUrl} className="h-8 w-8 md:w-8 md:h-8 mr-2" />}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
--- types.d.ts
+++ types.d.ts
@@ -7,7 +7,7 @@ export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
 };
 
-export type NextApiResponseSeverIo = NextApiResponse & {
+export type NextApiResponseServerIo = NextApiResponse & {
   socket: Socket & {
     server: NetServer & {
       io: SocketIoServer;
