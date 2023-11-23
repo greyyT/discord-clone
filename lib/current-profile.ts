e9eddcaad5921e903990ea8cf9fc2df4e03df8e2
greyyT <nhannguyen8338@gmail.com>
2023-11-23 13:46:28 +0700

    Modify current-profile util to get the current profile on the pages dir

--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -1,9 +1,11 @@
 import { auth } from '@clerk/nextjs';
+import { getAuth } from '@clerk/nextjs/server';
 
 import { db } from '@/lib/db';
+import { NextApiRequest } from 'next';
 
-export const currentProfile = async () => {
-  const { userId } = auth();
+export const currentProfile = async (req?: NextApiRequest) => {
+  const { userId } = req ? getAuth(req) : auth();
 
   if (!userId) {
     return null;
