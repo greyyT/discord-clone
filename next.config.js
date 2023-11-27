f2a37db8e8abcddf81801b4747c927bba95dc72b
greyyT <nhannguyen8338@gmail.com>
2023-11-27 16:51:00 +0700

    Modify next config to work with livekit (the problem only appears in next14)

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
